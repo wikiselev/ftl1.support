a6b49beaa59c7af7539d9f39f7c3b47f691fd519
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2014-07-25 00:33:15 +0100

    added some new files to back up; updated signatures.txt, smi.txt

--- process-gmail-text.pl
+++ process-gmail-text.pl
@@ -12,6 +12,7 @@ foreach ( @lines ) {
 	$_ =~ s/You have a new message: Via:  http:\/\/www.pravdina.info\/Message Details: Имя :  //;
 	$_ =~ s/You have a new message: Via:  http:\/\/www.pravdina.info\/Message Details: Имя и фамилия :  //;
 	$_ =~ s/Электронная почта\s*:\s*(.*)Год выпуска из ФТЛ (.*)1 \(если учились\)\s*:\s*/, /;
+	$_ =~ s/Электронная почта\s*:\s*(.*)Год выпуска из ФТЛ (.*)1\s:\s*/, /;
 	$_ =~ s/Место проживания :  /, /;
 	$_ =~ s/Место работы :  /, /;
 	$_ =~ s/Сообщение \(появится в подписях под письмом\) :  /\n/;
