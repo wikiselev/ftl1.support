626a760ae2cc2933f581bc74413a500b1a5bacfe
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2014-09-03 00:06:36 +0100

    added styles to pravdina.info using bower-wiredep

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,7 +164,7 @@ module.exports = function (grunt) {
     wiredep: {
       app: {
         ignorePath: /^\/|\.\.\//,
-        src: ['<%= config.app %>/index.html'],
+        src: ['<%= config.app %>/index.html', '<%= config.app %>/pravdina.info/index.html'],
         exclude: ['bower_components/bootstrap/dist/js/bootstrap.js']
       }
     },
